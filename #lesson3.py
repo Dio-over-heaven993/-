ec770a992ca26a6c383c52c7b860adf4c641382a
Арсен <arsenk10011@gmail.com>
2024-10-04 10:09:24 +0300

    Add files via upload

---
+++ #lesson3.py
@@ -0,0 +1,14 @@
+# n = int(input("Цена конфет за 1 кг "))
+# for i in range(2, 11):
+#     print("Цена за", i,"кг конфет равна", n*i,"рублей")
+
+#1  
+n = int(input("Введите верхний предел:" ))
+sum = 0
+count = 0
+for x in range(1, n+1):
+    x**3
+    print(x**3)
+    sum += x
+    count += 1
+#2
