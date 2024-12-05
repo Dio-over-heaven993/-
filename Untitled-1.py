deae6d8361742081fda0dcc017cb53639f164f08
Арсен <arsenk10011@gmail.com>
2024-12-05 17:30:12 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,28 @@
+#1
+import math
+R=  float(input("R= "))
+C=2*math.pi*R
+print(round(C, 2))
+
+S= math.pi*(R**2)
+print(round(S ,2))
+#2
+x=10
+y= 55
+print(x, y)
+x,y =y,x
+print(x,y)
+
+#3
+L=float(input("L= "))
+g = 9.81
+T = 2*math.pi*math.sqrt(L/g)
+print(round(T /2))
+
+
+ #if a > b >c :
+   # print('a > b >c')
+ # if b > a > c:
+ #   print('b > a > c')  
+  #if c > a > b:
+    #print('c > a > b') 
